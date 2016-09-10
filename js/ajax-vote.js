aa7a88dcdae1c3ba4b03469ebf57055ee52f392f
Wiput Pootong <wiput.pootong@gmail.com>
2016-09-10 19:44:43 +0700

    Update for sweetalert2

--- js/ajax-vote.js
+++ js/ajax-vote.js
@@ -18,8 +18,7 @@ function vote () {
 				else {
 					swal({
 						title: "กรุณาตรวจสอบข้อมูลให้ถูกต้อง",
-						text: "<img src='https://cdn.wiput.me/acsp/" + id + ".jpg' style='max-width: 40% !important;'><br><br>เลขประจำตัว : " + id + "<br>ชื่อ : " + result + "<br><b>หากโหวตแล้วจะไม่สามารถแก้ไขข้อมูลได้</b>",
-						html: true,
+						html: "<img src='https://cdn.wiput.me/acsp/" + id + ".jpg' style='max-width: 40% !important;'><br><br>เลขประจำตัว : " + id + "<br>ชื่อ : " + result + "<br><b>หากโหวตแล้วจะไม่สามารถแก้ไขข้อมูลได้</b>",
 						type: "warning",
 						showCancelButton: true,
 						confirmButtonColor: "#A5DC86",
@@ -27,34 +26,33 @@ function vote () {
 						cancelButtonText: "แก้ไขข้อมูล",
 						closeOnConfirm: false,
 						closeOnCancel: false,
-						showLoaderOnConfirm: true }, function(isConfirm){
-							if (isConfirm) {
-								$.ajax({
-									type: 'POST',
-									url: 'vote.php',
-									cache: false,
-									data: { 'id' : id , 'style' : style },
-									success: function(result){
-										switch (result) {
-											case "success" :
-												setTimeout(function(){
-													swal("โหวตสำเร็จ", "ขอบคุณสำหรับความร่วมมือครับ :D", "success");
-												}, 500);
-												break;
-											case "failed" :
-												setTimeout(function(){
-													swal("โหวตไม่สำเร็จ", "ห้ามโหวตซ้ำนะครับ :(", "error");
-												}, 500);
-										}
+						showLoaderOnConfirm: true }).then(function() {
+							$.ajax({
+								type: 'POST',
+								url: 'vote.php',
+								cache: false,
+								data: { 'id' : id , 'style' : style },
+								success: function(result){
+									switch (result) {
+										case "success" :
+										setTimeout(function(){
+											swal("โหวตสำเร็จ", "ขอบคุณสำหรับความร่วมมือครับ :D", "success");
+										}, 500);
+										break;
+										case "failed" :
+										setTimeout(function(){
+											swal("โหวตไม่สำเร็จ", "ห้ามโหวตซ้ำนะครับ :(", "error");
+										}, 500);
 									}
-								});
-						} else {
-							swal("ยกเลิกแล้ว", "กรุณาตรวจสอบข้อมูลใหม่ดีๆนะครับ :)", "error");
-						} 
-					});
+								}
+							});
+						}, function(dismiss) {
+							if (dismiss === 'cancel') {
+								swal("ยกเลิกแล้ว", "กรุณาตรวจสอบข้อมูลใหม่ดีๆนะครับ :)", "error");
+							}
+						});
+					}
 				}
-			}
-		});
+			});
 	}
-	
 }
