f59354afa877acb91991c0e70cea4910ea88c5f2
Wiput Pootong <wiput.pootong@gmail.com>
2016-09-13 23:22:59 +0700

    Pre-Release 2.0
    - Add Result Page
    - Edit Logo

---
+++ result.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>ACSP Product Design #4</title>
+	<link rel="stylesheet" href="css/custom.css" />
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="css/sweetalert2.min.css">
+	<meta charset="utf-8" />
+	<meta name="description" content="ร่วมโหวตเสื้อกันหนาวโรงเรียนในโครงการ ACSP Product Design ปีที่ 4" />
+	<meta name="author" content="สภานักเรียนอัสสัมชัญสมุทรปราการ"/>
+	<meta property="og:type" content="website"/>
+	<meta property="og:author" content="https://www.facebook.com/ACPowerParty"/>
+	<meta property="og:image" content="http://acspsc.com/img/og_image.png"/>
+	<meta property="og:url" content="http://acspsc.com/"/>
+	<meta name="theme-color" content="#ff1e00"/>
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+	<link rel="icon" href="img/logo.png">
+	<!-- CODED BY Wiput Development & CasperX Dev -->
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row text-center">
+			<h1>ผลโหวต ACSP Product Design #4</h1>
+		</div>
+		<div class="row">
+		<div class="col-md-10 col-md-offset-1">
+				<canvas id="ResultChart"></canvas>
+			</div>
+			
+		</div>
+	</div>
+	
+	<script src="js/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+	<script src="js/sweetalert2.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.2.2/Chart.min.js"></script>
+	<script src="js/result-get.js"></script>
+</body>
+</html>
---
+++ result_get.php
@@ -0,0 +1,17 @@
+<?
+	header('Content-Type: application/json');
+	include 'dbc.php';
+
+	$stmt = $conn->query("SELECT count(id) AS num_id, style FROM vote GROUP BY style");
+	$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+	$result = [];
+
+	foreach ($data as $key => $value) {
+		$result[$key]['num_id'] = (int) $value['num_id'];
+		$result[$key]['style'] = (int) $value['style'];
+	}
+
+	//Create Array
+	echo json_encode($result);
+?>
